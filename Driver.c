b9b60a6e760a670348acc2a0bd3f88f85547e406
kaveO <alban.billiette@gmail.com>
2025-02-18 06:57:34 +0100

    + | Finished Driver, have to setup Debugging and make README

--- Driver.c
+++ Driver.c
@@ -20,7 +20,7 @@ DriverEntry(
 	// KdPrintEx is a macro that sends string to the Kernel Debugger
 	// DPFLTR_IHVDRIVER_ID -> Used to associate the print of Driver as a something not offical "Handmade Driver" (Thats help filter messages in Debuggers)
 	// DPFLTR_INFO_LEVEL -> Means we are printing something at the Info Level (Thats help filter messages in Debuggers)
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "Hello Kernel World: DriverEntry\n"));
 	
 	// Init the driver config allocated above
 	// Add the entry point of Event Device Add callback	"KmdfHelloWordDeviceAdd"
@@ -30,4 +30,28 @@ DriverEntry(
 	status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
 
 	return (status);
-}	
+}
+
+NTSTATUS
+KmdfHelloWordDeviceAdd(
+	_In_	WDFDRIVER		Driver,
+	_Inout_ PWDFDEVICE_INIT	DeviceInit
+)
+{
+	// Not using Driver object so we unreference it (void)
+	UNREFERENCED_PARAMETER(Driver);
+
+	NTSTATUS status;
+
+	// Allocated device object like we did with Driver config above
+	WDFDEVICE hDevice;
+
+	// KdPrintEx is a macro that sends string to the Kernel Debugger
+	// DPFLTR_IHVDRIVER_ID -> Used to associate the print of Driver as a something not offical "Handmade Driver" (Thats help filter messages in Debuggers)
+	// DPFLTR_INFO_LEVEL -> Means we are printing something at the Info Level (Thats help filter messages in Debuggers)
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "Hello Kernel World: KmdfHelloWordDeviceAdd\n"));
+
+	status = WdfDeviceCreate(&DeviceInit, WDF_NO_OBJECT_ATTRIBUTES, &hDevice);
+	
+	return (status);
+}
